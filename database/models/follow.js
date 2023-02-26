355119c09b762ae1c4518170c293e4ec53b838ae
Ayrton Santos <ayrton.arc@gmail.com>
2023-02-26 14:01:22 -0800

    migrations follow

---
+++ database/models/follow.js
@@ -0,0 +1,32 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class Follow extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+    }
+  }
+  Follow.init({
+    user: DataTypes.STRING,
+    followed: DataTypes.STRING,
+    createdAt: {
+      type: "TIMESTAMP",
+      allowNull: false,
+      field: "createdAt",
+      defaultValue: sequelize.literal("CURRENT_TIMESTAMP"),
+    }
+
+  }, {
+    sequelize,
+    modelName: 'Follow',
+    tableName: 'Follows'
+  });
+  return Follow;
+};
