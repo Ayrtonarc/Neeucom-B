c4d8c688f442ca440cc031d418aecf8758262cc4
Ayrton Santos <ayrton.arc@gmail.com>
2023-03-02 20:01:55 -0800

    relations

--- database/models/follow.js
+++ database/models/follow.js
@@ -11,11 +11,16 @@ module.exports = (sequelize, DataTypes) => {
      */
     static associate(models) {
       // define association here
+      Follow.belongsTo(models.User, {foreignKey: 'userId', as: 'follow'});
+      Follow.belongsTo(models.User, {foreignKey: 'followedId', as: 'followed'});
     }
   }
   Follow.init({
+
     user: DataTypes.STRING,
+
     followed: DataTypes.STRING,
+
     createdAt: {
       type: "TIMESTAMP",
       allowNull: false,
