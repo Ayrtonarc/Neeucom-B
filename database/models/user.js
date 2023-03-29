a1a2a36c149a66a2df29bbbb563a356dc291d00f
Ayrton Santos <ayrton.arc@gmail.com>
2023-03-28 20:26:02 -0700

    correccion a modelo de clases en user

--- database/models/user.js
+++ database/models/user.js
@@ -2,40 +2,50 @@
 
 
 const bcrypt = require('bcryptjs');
-
+const { Model } = require('sequelize');
 module.exports = (sequelize, DataTypes) => {
-  const User = sequelize.define('User', {
+  class User extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+      User.hasMany(models.Follow, { foreignKey: 'user', as: 'follow' });
+      User.hasMany(models.Follow, { foreignKey: 'followed', as: 'followed' });
+    }
+
+    async generatePasswordHash() {
+      if (this.password) {
+        return bcrypt.hash(this.password, 10);
+      }
+    }
+  }
+
+  User.init({
     firstname: {
       type: DataTypes.STRING,
       allowNull: true,
       field: "firstname",
       defaultValue: ''
     },
-      lastname: DataTypes.STRING,
-      username: DataTypes.STRING,
-      bio: DataTypes.STRING,
-      email: DataTypes.STRING,
-      password: DataTypes.STRING,
+    lastname: DataTypes.STRING,
+    username: DataTypes.STRING,
+    bio: DataTypes.STRING,
+    email: DataTypes.STRING,
+    password: DataTypes.STRING,
+  }, {
+    sequelize,
+    modelName: 'User',
+    defaultScope: {
+      rawAttributes: { exclude: ['password'] },
     },
-    {
-      defaultScope: {
-        rawAttributes: { exclude: ['password'] },
-      },
-    },
-  );
+  });
 
   User.beforeCreate(async (user) => {
     user.password = await user.generatePasswordHash();
   });
-  User.prototype.generatePasswordHash = function () {
-    if (this.password) {
-      return bcrypt.hash(this.password, 10);
-    }
-  };
-  User.associate = function (models) {
-    // associations can be defined here
-    User.hasMany(models.Follow, {foreignKey: 'user', as: 'follow'})
-    User.hasMany(models.Follow, {foreignKey: 'followed', as: 'followed'})
-  };
+
   return User;
 };
